4fbcabfc04cc7513fcd6aa258a5755772393a98a
Przemek Tazbirek <przemek10197@gmail.com>
2020-03-21 17:27:23 +0100

    zrobione cwiczenia 1-7 oraz 15

---
+++ ZPO_cwiczenia4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZPO_cwiczenia4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
